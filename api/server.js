1f86932e44247dc59ba8622ac29f359e582df1dc
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-05-20 12:51:52 -0700

    updates index & dependencies per review vid

--- api/server.js
+++ api/server.js
@@ -1,1 +1,14 @@
 // build your server here and require it from index.js
+
+const express = require('express');
+
+//const fruitsRouter = require('./fruits/fruits-router.js');
+
+const server = express();
+
+server.use(express.json());
+
+server.use('/api/resource/router', projectsRouter);
+
+module.exports = server;
+
