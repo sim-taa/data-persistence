e38c960c9f128d71a2b688e7c8ce78b4e911256a
Simone Taarnskov <simone.taarnskov@gmail.com>
2022-06-20 11:43:54 -0700

    set up error handling in project router

--- api/project/model.js
+++ api/project/model.js
@@ -1,49 +1,16 @@
 // build your `Project` model here
 const db = require("../../data/dbConfig.js");
+const mappers = require("../../data/helpers/mappers.js");
+
 module.exports = {
   get,
   insert,
-  // update,
-  // remove,
-  // getProjectActions,
-  // intToBoolean,
-  // booleanToint,
-  // projectToBody,
-  // actionToBody,
 };
 
-function get() {}
+function get(project_id) {}
 
 function insert(project) {
-  return db("projects").insert(project);
+  return db("projects")
+    .insert(project)
+    .then(([project_id]) => get(id));
 }
-//   function intToBoolean(int) {
-//     return int === 1 ? true : false;
-//   }
-
-//   function booleanToint(bool) {
-//     return bool === true ? 1 : 0;
-//   }
-
-//   function projectToBody(project) {
-//     const result = {
-//       ...project,
-//       completed: intToBoolean(project.completed),
-//     };
-
-//     if (project.actions) {
-//       result.actions = project.actions.map(action => ({
-//         ...action,
-//         completed: intToBoolean(action.completed),
-//       }));
-//     }
-
-//     return result;
-//   }
-
-//   function actionToBody(action) {
-//     return {
-//       ...action,
-//       completed: intToBoolean(action.completed),
-//      };
-//   }
